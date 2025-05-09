0428a8d54bbdde34adc223c087b7cf48a4e0b9d7
Anton Khamets <anton.khamets@hey.com>
2025-05-10 00:09:50 +0500

    Also add import when adding hook

--- insert-when-missing.rb
+++ insert-when-missing.rb
@@ -6,36 +6,24 @@ class CLI < Thor
   desc "add_import TRIGGER IMPORT", "Adds import" 
   def add_import(trigger, import)
     files(trigger, import).each do |filename|
-      imports, *rest = File.read(filename).split(/^$/)
-
       puts "#{filename} y/n?"
       if $stdin.getch != "y" then next end
 
-      File.write filename, [imports, import, "\n", *rest].join("")
+      insert_import filename, import
     end
   end
 
-  desc "add_hook TRIGGER HOOK", "Adds import" 
-  def add_hook(trigger, hook)
+  desc "add_hook TRIGGER HOOK [IMPORT]", "Adds import" 
+  def add_hook(trigger, hook, import)
     files(trigger, hook).each do |filename|
-      lines = File.read(filename).split("\n")
-
-      dispatch_hook = lines.detect { |line| line.include? "const dispatch = " }
-      translate_hook = lines.detect { |line| line.include? "const [translate] = " }
-      function_component_start = lines.detect { |line| line.start_with? "export default function " }
-      const_component_start = lines.detect { |line| line.start_with? "const " }
-      index = lines.index(dispatch_hook || translate_hook || function_component_start || const_component_start)
-
-      if not index then next end
-
       puts "#{filename} y/n?"
       if $stdin.getch != "y" then next end
 
-      File.write filename, [
-        *lines.slice(0..index), 
-        "  " + hook, 
-        *lines.slice(index+1..)
-      ].join("\n")
+      insert_hook filename, hook
+
+      unless not import or File.read(filename).include? import
+        insert_import filename, import
+      end
     end
   end
 
@@ -43,6 +31,29 @@ class CLI < Thor
 
   def files(trigger, missing)
     `grep -rl '#{trigger}' src | xargs grep -L '#{missing}'`.split("\n")
+  end
+
+  def insert_import(filename, import)
+    imports, *rest = File.read(filename).split(/^$/)
+    File.write filename, [imports, import, "\n", *rest].join("")
+  end
+
+  def insert_hook(filename, hook)
+    lines = File.read(filename).split("\n")
+
+    dispatch_hook = lines.detect { |line| line.include? "const dispatch = " }
+    translate_hook = lines.detect { |line| line.include? "const [translate] = " }
+    function_component_start = lines.detect { |line| line.start_with? "export default function " }
+    const_component_start = lines.detect { |line| line.start_with? "const " }
+    index = lines.index(dispatch_hook || translate_hook || function_component_start || const_component_start)
+
+    if not index then return end
+
+    File.write filename, [
+      *lines.slice(0..index), 
+      "  " + hook, 
+      *lines.slice(index+1..)
+    ].join("\n")
   end
 end
 
